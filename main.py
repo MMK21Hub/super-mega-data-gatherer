261777837b58b467a6596aa96d536ab3da1e92b8
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-02-25 16:48:02 +0000

    Revert "Maybe fix it for real this time?"
    This reverts commit 6b3c4f0973e2da8f69efd3d2123a2ab9fa9464e9.

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ db_client = DatabaseClient()
 async def app_lifespan(_: FastAPI):
     global db_client
     logger.info(f"Starting {PROJECT_NAME}", event_id="startup_start")
-    # Any future initialisation goes here
+    await db_client.open_pool()
     logger.info(f"{PROJECT_NAME} is ready", event_id="startup_complete")
     yield
     logger.info(f"Shutting down {PROJECT_NAME}", event_id="shutdown_start")
-    # Any future cleanup goes here
+    await db_client.close_pool()
     logger.info(f"{PROJECT_NAME} has shut down", event_id="shutdown_complete")
 
 
