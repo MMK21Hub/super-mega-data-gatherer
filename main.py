e8d9e3b6cd95177674a00514fb4f3d56d338585a
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-03-04 10:47:34 +0000

    We have caching now

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import uuid
 from database_stats import DatabaseClient
 from logging_config import configure_logging
 from prometheus_stats import get_unresolved_tickets
+from stats_store import SuperMegaStatsManager
 
 PROJECT_NAME = "Super Mega Data Gatherer"
 
@@ -20,49 +21,32 @@ configure_logging()
 logger = get_logger()
 logger.info(f"Initialising {PROJECT_NAME}", event_id="initialisation")
 
-db_client = DatabaseClient()
+stats_manager = SuperMegaStatsManager()
 
 
 @asynccontextmanager
 async def app_lifespan(_: FastAPI):
     global db_client
     logger.info(f"Starting {PROJECT_NAME}", event_id="startup_start")
-    await db_client.open_pool()
+    # Any future initialisation can go here
     logger.info(f"{PROJECT_NAME} is ready", event_id="startup_complete")
     yield
     logger.info(f"Shutting down {PROJECT_NAME}", event_id="shutdown_start")
-    await db_client.close_pool()
+    # Any future cleanup can go here
     logger.info(f"{PROJECT_NAME} has shut down", event_id="shutdown_complete")
 
 
 app = FastAPI(lifespan=app_lifespan)
 
 
-async def get_hang_time_data(
-    start: datetime, end: datetime
-) -> dict[str, dict[str, float]] | None:
-    try:
-        return {
-            "p90": await db_client.get_question_hang_times(start, end, 0.90),
-            "p95": await db_client.get_question_hang_times(start, end, 0.95),
-        }
-    except Exception as e:
-        logger.error(
-            "Failed to fetch question hang time data",
-            error=str(e),
-            exc_info=exc_info(),
-        )
-        return None
-
-
 @app.get("/api/v1/super-mega-stats")
 async def super_mega_stats(start: datetime, end: datetime | None):
     end = end or datetime.now(UTC).replace(microsecond=0)
+
+    stats = await stats_manager.get_stats(start, end)
     return {
-        "unresolved_tickets": await get_unresolved_tickets(
-            start, end, step=timedelta(days=1)
-        ),
-        "hang_time": await get_hang_time_data(start, end),
+        "unresolved_tickets": stats.unresolved_tickets_data,
+        "hang_time": stats.hang_time_data,
     }
 
 
@@ -77,9 +61,7 @@ async def root(request: Request):
 
 @app.get("/health")
 async def health_check():
-    healths = {"database": await db_client.is_healthy()}
-    overall_health = all(healths.values())
-    return {"ok": overall_health, **healths}
+    return await stats_manager.health_check()
 
 
 @app.middleware("http")
---
+++ stats_store.py
@@ -0,0 +1,82 @@
+"""Handles generating, caching, and string the stats"""
+
+from datetime import date, datetime, timedelta
+
+from database_stats import DatabaseClient
+from prometheus_stats import get_unresolved_tickets
+
+
+type HangTimeData = dict[str, dict[str, float]]
+type UnresolvedTicketsData = dict[str, int]
+
+
+class SuperMegaStatsStore:
+    def __init__(
+        self,
+        hang_time_data: HangTimeData | None = None,
+        unresolved_tickets_data: UnresolvedTicketsData | None = None,
+    ):
+        self._hang_time_data: HangTimeData | None = hang_time_data
+        self._unresolved_tickets_data: UnresolvedTicketsData | None = (
+            unresolved_tickets_data
+        )
+        self.updated_at: datetime | None = (
+            datetime.now() if (hang_time_data or unresolved_tickets_data) else None
+        )
+
+    @property
+    def hang_time_data(self) -> HangTimeData | None:
+        return self._hang_time_data
+
+    @hang_time_data.setter
+    def hang_time_data(self, value: HangTimeData):
+        self._hang_time_data = value
+        self.updated_at = datetime.now()
+
+    @property
+    def unresolved_tickets_data(self) -> UnresolvedTicketsData | None:
+        return self._unresolved_tickets_data
+
+    @unresolved_tickets_data.setter
+    def unresolved_tickets_data(self, value: UnresolvedTicketsData):
+        self._unresolved_tickets_data = value
+        self.updated_at = datetime.now()
+
+
+class SuperMegaStatsManager:
+    def __init__(self):
+        # A map of cache keys (which are (start, end) tuples) to stats stores
+        self.stats_cache: dict[tuple[datetime, datetime], SuperMegaStatsStore] = dict()
+        self._db_client = DatabaseClient()
+
+    async def db_client(self) -> DatabaseClient:
+        await self._db_client.open_pool()
+        return self._db_client
+
+    async def health_check(self) -> dict[str, bool]:
+        db = await self.db_client()
+        healths = {"database": await db.is_healthy()}
+        overall_health = all(healths.values())
+        return {"ok": overall_health, **healths}
+
+    async def get_stats(self, start: datetime, end: datetime) -> SuperMegaStatsStore:
+
+        cache_key = (start, end)
+        if cache_key in self.stats_cache:
+            store = self.stats_cache[cache_key]
+            # Consider entries stale if they're from a previous day
+            if store.updated_at and store.updated_at.date() == date.today():
+                return store
+
+        # No cache entry, so data must be fetched
+        db = await self.db_client()
+        store = SuperMegaStatsStore()
+        store.unresolved_tickets_data = await get_unresolved_tickets(
+            start, end, step=timedelta(days=1)
+        )
+        store.hang_time_data = {
+            "p90": await db.get_question_hang_times(start, end, 0.90),
+            "p95": await db.get_question_hang_times(start, end, 0.95),
+        }
+        self.stats_cache[cache_key] = store
+        return store
